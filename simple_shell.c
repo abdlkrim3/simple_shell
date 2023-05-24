fe246f57cb6574034330e6cbee8cce3c23eda632
Benalla-Aiman <benallaaiman@gmailcom>
2023-05-24 14:31:50 -0700

    simple_shell example

---
+++ simple_shell.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#define MAX_CMD_LEN 1024
+#define PROMPT "#cisfun$ "
+/**
+* main - simple UNIX command line interpreter
+*
+* Return: 0 on success, 1 on error
+*/
+int main(void)
+{
+char cmd[MAX_CMD_LEN];
+char *args[] = {NULL, NULL};
+int status;
+while (1)
+{
+printf(PROMPT);
+if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL)
+{
+printf("\n");
+exit(0);
+}
+cmd[strcspn(cmd, "\n")] = '\0';
+args[0] = cmd;
+if (fork() == 0)
+{
+execve(args[0], args, NULL);
+perror(args[0]);
+exit(1);
+}
+else
+{
+wait(&status);
+}
+}
+return (0);
+}
