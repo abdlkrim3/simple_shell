3fb02bb725ba1030b3127a3c8f134a7f17e37965
abdlkrim3 <abdlkrim.agoujil@gmail.com>
2023-05-24 16:36:46 -0700

    simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -2,39 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#define MAX_CMD_LEN 1024
-#define PROMPT "#cisfun$ "
+
+#define MAX_COMMAND_LENGTH 100
+
 /**
-* main - simple UNIX command line interpreter
-*
-* Return: 0 on success, 1 on error
-*/
+ * main - Entry point of the shell program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char cmd[MAX_CMD_LEN];
-char *args[] = {NULL, NULL};
-int status;
-while (1)
-{
-printf(PROMPT);
-if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL)
-{
-printf("\n");
-exit(0);
-}
-cmd[strcspn(cmd, "\n")] = '\0';
-args[0] = cmd;
-if (fork() == 0)
-{
-execve(args[0], args, NULL);
-perror(args[0]);
-exit(1);
-}
-else
-{
-wait(&status);
-}
-}
-return (0);
+	char command[MAX_COMMAND_LENGTH];
+	char prompt[] = "#cisfun$ ";
+
+	while (1)
+	{
+		printf("%s", prompt);
+
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			else
+			{
+				perror("fgets");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		command[strcspn(command, "\n")] = '\0';
+
+		char *token = strtok(command, " ");
+		if (token == NULL)
+		{
+			continue;
+		}
+
+		pid_t pid = fork();
+
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0) // Child process
+		{
+			if (execlp(token, token, (char *)NULL) == -1)
+			{
+				perror("execlp");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else // Parent process
+		{
+			int status;
+			waitpid(pid, &status, 0);
+			printf("\n");
+		}
+
+		if (access(token, F_OK) == -1)
+		{
+			printf("Command '%s' not found\n", token);
+		}
+	}
+
+	return 0;
 }
